e375f1dd02a15f7fdf82a3b25aff861b94fbb294
keith <keith@host.docker.internal>
2024-02-19 07:39:49 -0500

    Update local URL for desktop docker grid

--- seleniumFramework/src/com/keithu9999/selenium/infra/SeleniumDriver.java
+++ seleniumFramework/src/com/keithu9999/selenium/infra/SeleniumDriver.java
@@ -44,7 +44,7 @@ public class SeleniumDriver {
 					DesiredCapabilities capabilities = new DesiredCapabilities();
 			        capabilities.setCapability(ChromeOptions.CAPABILITY, opts);
 			        try {
-			        	driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
+			        	driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), capabilities);
 			        } catch (MalformedURLException ex) {
 			        	logger.warning(ex.toString());
 			        }
